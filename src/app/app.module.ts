4a1f0b47b4e904f5d61e9d641f559c7dc62bdd7f
carlos-dubon <holacarlosdubon@gmail.com>
2021-08-08 11:39:16 -0600

    🗚 Added Inter font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './main/pages/home/home.component';
+import { FooterComponent } from './main/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
