03a8eb96f40d00076d90e5dd2169e67cb32af369
carlos-dubon <holacarlosdubon@gmail.com>
2021-08-08 14:34:05 -0600

    🥾 Footer component

--- src/app/main/shared/components/footer/footer.component.ts
+++ src/app/main/shared/components/footer/footer.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSingletonProps } from 'ngx-tippy-wrapper';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  singletonTransitions: NgxSingletonProps = {
+    animation: 'shift-away',
+    interactiveBorder: 30,
+    interactiveDebounce: 75,
+    theme: 'dark',
+    offset: [0, 15],
+    arrow: true,
+    moveTransition: 'transform 0.4s ease-in-out',
+  };
 }
