e12352c8ecc7d3728291977b0a49ad7f89c6a35a
carlos-dubon <holacarlosdubon@gmail.com>
2021-08-08 12:49:43 -0600

    🤯 Header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './main/pages/home/home.component';
-import { FooterComponent } from './main/shared/footer/footer.component';
+import { FooterComponent } from './main/shared/components/footer/footer.component';
+import { HeaderComponent } from './main/shared/components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, FooterComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
