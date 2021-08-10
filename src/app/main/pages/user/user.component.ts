f852b6394389e4fa408f4b34bd4b365b1dd1b623
carlos-dubon <holacarlosdubon@gmail.com>
2021-08-10 15:36:28 -0600

    :sparkles: get id param

--- src/app/main/pages/user/user.component.ts
+++ src/app/main/pages/user/user.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent implements OnInit {
+  public id: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.id = params['id'];
+    });
   }
-
 }
