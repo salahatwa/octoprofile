f5a94199c51de2d672c6362c9a2ffa89ed614718
carlos-dubon <holacarlosdubon@gmail.com>
2021-08-10 21:06:03 -0600

    🔥 Positioned the stats polygons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { NgxTippyModule } from 'ngx-tippy-wrapper';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UserSearchComponent } from './main/shared/components/user-search/user-search.component';
 import { UserComponent } from './main/pages/user/user.component';
+import { MainStatsComponent } from './main/pages/user/main-stats/main-stats.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, FooterComponent, HeaderComponent, UserSearchComponent, UserComponent],
+  declarations: [AppComponent, HomeComponent, FooterComponent, HeaderComponent, UserSearchComponent, UserComponent, MainStatsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/main/pages/user/main-stats/main-stats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-stats',
+  templateUrl: './main-stats.component.html',
+  styleUrls: ['./main-stats.component.scss']
+})
+export class MainStatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
