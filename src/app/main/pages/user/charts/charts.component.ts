6948e0dc4bce4f78c65d8829d330605af7deff19
carlos-dubon <holacarlosdubon@gmail.com>
2021-08-13 18:11:18 -0600

    ⚾ pagination request to the api

--- src/app/main/pages/user/charts/charts.component.ts
+++ src/app/main/pages/user/charts/charts.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from './../../../services/user.service';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
-  styleUrls: ['./charts.component.scss']
+  styleUrls: ['./charts.component.scss'],
 })
 export class ChartsComponent implements OnInit {
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {}
 
-  constructor() { }
+  public repositories: any[] = [];
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      const id: string = params['id'];
+
+      this.userService.getUserRepos(id).subscribe(
+        (repos: any[]) => {
+          this.repositories.push(...repos);
+        },
+        (err) => {
+          console.error(err);
+        },
+        () => {
+          console.log('Complete');
+          console.log(this.repositories);
+        }
+      );
+    });
   }
 
+  public getLanguagePerRepoList(repos: any[]): string[] {
+    const arr: string[] = [];
+
+    repos.forEach((repo: any) => {
+      arr.push(repo.language);
+    });
+
+    return arr.sort();
+  }
 }
--- src/app/main/services/user.service.ts
+++ src/app/main/services/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
 import { UserI } from '../models/user.model';
 
 @Injectable({
@@ -11,5 +11,23 @@ export class UserService {
 
   public getUserData(user: string): Observable<UserI> {
     return this.http.get<UserI>(`https://api.github.com/users/${user}`);
+  }
+
+  public getUserRepos(user: string): Observable<any[]> {
+    return new Observable((subscriber: Subscriber<any[]>) => {
+      this.getUserData(user).subscribe((data: UserI) => {
+        const pages: number = Math.ceil(data.public_repos / 100);
+
+        for (let i = 1; i <= pages; i++) {
+          this.http
+            .get(
+              `https://api.github.com/users/${user}/repos?page=${i}&per_page=100`
+            )
+            .subscribe((data: any[]) => {
+              subscriber.next(data);
+            });
+        }
+      });
+    });
   }
 }
