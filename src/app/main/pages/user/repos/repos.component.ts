fecc2b9ca7ab95563f6509f281df1353ae047b0a
carlos-dubon <holacarlosdubon@gmail.com>
2021-09-07 12:44:25 -0600

    🧾 Working on repos filter

--- src/app/main/pages/user/repos/repos.component.ts
+++ src/app/main/pages/user/repos/repos.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
-  styleUrls: ['./repos.component.scss']
+  styleUrls: ['./repos.component.scss'],
 })
 export class ReposComponent implements OnInit {
+  currentFilter: string = 'Stars';
 
-  constructor() { }
+  reposForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.reposForm = this.fb.group({
+      input: [''],
+    });
   }
 
+  ngOnInit(): void {}
+
+  resetInput(): void {
+    this.reposForm.get('input').reset();
+  }
 }
